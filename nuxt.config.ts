2d8c6f00c36b031283870f1198369d4039c40016
Ivan Kuzmin <kuzmin6969@gmail.com>
2022-08-10 17:34:23 +0300

    Upgrade node version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 import { defineNuxtConfig } from 'nuxt'
 
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+import Icons from 'unplugin-icons/vite'
+
 export default defineNuxtConfig({
-    buildModules: [
-        ['unplugin-icons/nuxt', { /* options */ }],
-    ],
+    vite: {
+        plugins: [
+            Icons({
+                // the feature below is experimental ⬇️
+                autoInstall: true
+            })
+        ]
+    }
 })
