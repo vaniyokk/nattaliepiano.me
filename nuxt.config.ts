ae804eea33ed594562d80da91eec2f6de51810a7
Ivan Kuzmin <kuzmin6969@gmail.com>
2022-08-10 17:29:05 +0300

    Fix github actions problem

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,7 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-import Icons from 'unplugin-icons/vite'
-
 export default defineNuxtConfig({
-    vite: {
-        plugins: [
-            Icons({
-                // the feature below is experimental ⬇️
-                autoInstall: true
-            })
-        ]
-    }
+    buildModules: [
+        ['unplugin-icons/nuxt', { /* options */ }],
+    ],
 })
